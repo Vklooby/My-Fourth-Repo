86954c1bde7bd5a34e34a635bffa36824c78e6fa
Vklooby <victoria.klooby@gmail.com>
2025-02-06 21:15:08 -0500

    Adding project to GitHub again

---
+++ src/test/java/org/example/Sample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Making Changes after pushing project");
+        System.out.println("Adding new line");
+    }
+}
---
+++ src/test/java/org/example/TestOne.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("Adding Test One");
+
+    }
+}
